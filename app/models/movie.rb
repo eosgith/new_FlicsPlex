58622648947b489753c6a1c82c98120e99a0bc5c
kennbyrne <ken@kenbyrne.com>
2018-04-01 23:56:51 +0000

    <cart added, via line item to return to. Buttons to be createdn>

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,24 @@
 class Movie < ActiveRecord::Base
+  default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  #...
+  
+  private
+  
+  
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+     return true
+  else
+    errors.add(:base, 'Line Items present')
+    return false
+    end
+  end
+  
+  
   def poster
     "http://ia.media-imdb.com/images/M/#{poster_url}"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
  
+  resources :line_items
+  resources :carts
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
    root 'movies#index'
   
