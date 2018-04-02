d546fb41253938f967db094497e3f46ef574e871
apope8 <apope8@gamil.com>
2018-04-02 18:20:59 +0000

    fix default scope error

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-  default_scope :order => 'title'
+  default_scope {where(color: 'red') }
   has_many :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
   
